a7c65374e430ee97a0607ff0224b98a3bbb4db4f
davienjo <66726094+davienjo@users.noreply.github.com>
2025-07-18 21:07:17 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,133 @@
+<?php
+include('partials/header.php');
+
+?>
+
+
+  <div class="hero">
+    <div class="hero-content">
+      <h1>Styles that speak</h1>
+      <p>
+        Premium haircuts, clean fades, and sharp beards<br />
+        — all in one chair.
+      </p>
+      <a class="btn-1" href="book.php">Book Appointment</a>
+    </div>
+  </div>
+  </header>
+
+  <section class="images-gallery">
+    <h2 class="h2-images">Gallery</h2>
+
+    <div class="image-slider">
+      <img src="images/pexels-cottonbro-3992874.jpg" alt="image-1" />
+      <img src="images/pexels-cottonbro-3993293.jpg" alt="image-2" />
+      <img src="images/pexels-lumierestudiomx-897253.jpg" alt="image-3" />
+      <img src="images/pexels-lumierestudiomx-897270 (1).jpg" alt="image-4" />
+      <img src="images/pexels-lumierestudiomx-897271.jpg" alt="image-5" />
+      <img src="images/pexels-mati-2528695.jpg" alt="image-6" />
+    </div>
+  </section>
+
+  <!-- testimonial section -->
+
+  <h2 class="h2-testimonials">Public cheers for us</h2>
+
+  <section class="testimonials">
+
+    <div class="testimonial-1">
+
+      <div class="img-section">
+        <img src="images/beard-1845166_640.jpg" alt="" />
+
+        <div class="particulars">
+          <span class="person-name">person's name</span>
+          <span>Profession</span>
+        </div>
+      </div>
+
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.Enim quisquam
+        ducimus quis reiciendis veniam corporis minus eos.
+      </p>
+    </div>
+
+    <div class="testimonial-1">
+      <div class="img-section">
+        <img src="images/fashion-4951644_640.jpg" alt="" />
+        <div class="particulars">
+          <span class="person-name">person's name</span>
+          <span>Profession</span>
+        </div>
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.Enim quisquam
+        ducimus quis reiciendis veniam corporis minus eos.
+      </p>
+    </div>
+
+    <div class="testimonial-1">
+      <div class="img-section">
+        <img src="images/human-3782189_640.jpg" alt="" />
+        <div class="particulars">
+          <span class="person-name">person's name</span>
+          <span>Profession</span>
+        </div>
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.Enim quisquam
+        ducimus quis reiciendis veniam corporis minus eos.
+      </p>
+    </div>
+
+    <div class="testimonial-1">
+      <div class="img-section">
+        <img src="images/man-4078827_640.jpg" alt="" />
+        <div class="particulars">
+          <span class="person-name">person's name</span>
+          <span>Profession</span>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.Enim quisquam
+        ducimus quis reiciendis veniam corporis minus eos.
+      </p>
+    </div>
+
+    <div class="testimonial-1">
+      <div class="img-section">
+        <img src="images/user-1.png" alt="" />
+        <div class="particulars">
+          <span class="person-name">person's name</span>
+          <span>Profession</span>
+        </div>
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.Enim quisquam
+        ducimus quis reiciendis veniam corporis minus eos.
+      </p>
+    </div>
+
+    <div class="testimonial-1">
+      <div class="img-section">
+        <img src="images/user-2.png" alt="" />
+        <div class="particulars">
+          <span class="person-name">person's name</span>
+          <span>Profession</span>
+        </div>
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.Enim quisquam
+        ducimus quis reiciendis veniam corporis minus eos.
+      </p>
+    </div>
+  </section>
+
+  <?php
+  include('partials/footer.php');
+  ?>
